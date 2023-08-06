81ce246b4fbd8518f16c907161b8e1753493f542
k336699575 <k606090857@gmail.com>
2023-08-06 08:59:49 +0800

    新

---
+++ test copy.py
@@ -0,0 +1,12 @@
+
+# n = 9
+# r = 1
+# while n > 0:
+#     r = r * n
+#     n = n - 1
+# print(r)   
+radius = 10 #radius 圓形半徑
+PI = 3.14
+print("圓形半徑",radius)
+print("圓形周長",2*PI*radius)
+print("圓形面積",PI*radius*radius)
