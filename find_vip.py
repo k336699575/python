f01590ff7bd6e1d3ea4275dd09bf769a0b4b9465
DAST-Macbook <jackie@dast.tw>
2023-08-23 21:09:48 +0800

    refactor: refactor variable names and assignments
    - Change the variable name `results` to `num`
    - Modify the cell assignment to use the new variable `num`
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

--- find_vip.py
+++ find_vip.py
@@ -5,12 +5,12 @@ wb = openpyxl.load_workbook('names_and_savings.xlsx')
 
 ws = wb['工作表1']
 
-results = 1
+num = 1
 
 for row in ws.iter_rows():
     numbers: int =row[2].value
     if numbers >= 20000:
-        ws.cell(row=results, column=4).value ="VIP"
+        ws.cell(row=num, column=4).value ="VIP"
     results += 1    
 # print(ws.cell(row=1, column=1).value)
 # print(ws.cell(row=1, column=2).value)
