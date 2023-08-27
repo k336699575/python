a9a2c80a6ca537ae82971f749291149cce537dea
k336699575 <k606090857@gmail.com>
2023-08-27 08:58:34 +0800

    if

--- test3.py
+++ test3.py
@@ -9,9 +9,14 @@ num = 1
 
 for row in ws.iter_rows():
     numbers =row[2].value
-    if numbers >= 20000:
+    if numbers >= 20000 :
+       ws.cell(row=num, column=4).value ="VVIP"
+    elif numbers  >=10000 : 
        ws.cell(row=num, column=4).value ="VIP"
-    num += 1   
+    else: 
+       ws.cell(row=num, column=4).value ="Standard" 
+        
+    num += 1  
    
 # print(ws.cell(row=num+1, column=1).value)
 # print(ws.cell(row=num+1, column=2).value)
